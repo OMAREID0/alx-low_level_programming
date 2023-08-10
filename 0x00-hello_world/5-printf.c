5b6cf1c9829788002d5c047becc4283086c81c7b
OMAREID0 <omareid221b@gmail.com>
2023-08-10 16:02:45 -0700

    c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,10 +4,11 @@
  *main - prints the phrase
  *Return: 0 if exited properly
  */
-       int main(void)
-{
 
-	puts ("\"Programming is like building a multilingual puzzle");
+int main(void) {
+
+printf("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
+
 
 }
