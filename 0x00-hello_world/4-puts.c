f84e06ed49cbafc97cd6c69b3d871798f7122415
OMAREID0 <omareid221b@gmail.com>
2023-08-10 15:50:32 -0700

    c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - print the line 
+ *Return: 0 if close
+ */
+       int main(void)
+{
+
+	puts ("\"Programming is like building a multilingual puzzle");
+	return(0);
+
+}
