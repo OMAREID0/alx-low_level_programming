8cb3db39a68021cec5f7705d8f666bf9a1d4e86b
OMAREID0 <omareid221b@gmail.com>
2023-08-11 18:54:46 -0700

    m

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,8 +4,7 @@
 
 
 /*
- *main - assigns a random number to int n everytime
- * it executes, and prints it
+ * main - assigns a random number to int n everytime it executes,and print it
  * Return: Always 0 (Success)
  */
 
