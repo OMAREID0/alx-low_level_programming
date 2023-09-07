f95f62a5f3e85aee7b2a8d5856b69aa4d0951454
OMAREID0 <omareid221b@gmail.com>
2023-09-07 13:30:36 -0700

    g

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/*
+ * create_array - function to creat an array of char with knowen size 
+ *and replace the empate spaces with char c
+ *@size - show how many unit will need to allocate.
+ *@c -the char that will be overwritten
+ *Return: return (NULL) if there is no free space and return array if it successful done
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	int i;
+	char *s;
+	s = malloc(size * sizeof(int));
+	if ( s == NULL)
+		return 0;
+	for(i = 0; i < size; ++i)
+	{
+		s[i] == c;
+	}
+	s[i] = NULL;
+	return s;
+}
