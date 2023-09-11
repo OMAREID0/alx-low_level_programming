6fc4c57dc2ab318d5a9d1c1cc89682fdedf4950a
OMAREID0 <omareid221b@gmail.com>
2023-09-11 11:20:56 +0300

    first try

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef 3-FUNCTION_LIKE_MACRO_H
+#define 3-FUNCTION_LIKE_MACRO_H
+#define ABS(x) (unsigned int(x))
+#endif
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,3 @@
+
+
+1.0-object_like_macro.h : header file that define SIZE as an abbreviation for the token 1024
