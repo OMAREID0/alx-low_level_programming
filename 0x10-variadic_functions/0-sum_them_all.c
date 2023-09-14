f8ffe94e416a577fb21cab44bbb8ca03ca984f51
OMAREID0 <omareid221b@gmail.com>
2023-09-15 01:29:03 +0300

    Betty style

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,25 +1,23 @@
 #include "variadic_functions.h"
 
 /**
- * sum_them_all - return sum of all parameters
- * @n: constant of namber of arguments
+ * sum_them_all - calculates the sum of all its parameters
+ * @n: number of arguments passed to the function
  *
- * Return: return (sum) when sucssed
- * return 0 if (n == 0)
+ * Return: the resulting sum
  */
-
 int sum_them_all(const unsigned int n, ...)
 {
-        int sum = 0;
 	unsigned int i;
-	va_list arg;
+	int sum = 0;
+	va_list list;
 
-        va_start(arg, n);
+	va_start(list, n);
 
-        for(i = 0 ; i < n ; i++)
-                sum += va_arg (arg, int);
-       
-	va_end(arg);       
+	for (i = 0; i < n; i++)
+		sum += va_arg(list, int);
+
+	va_end(list);
 
 	return (sum);
 }
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,34 +1,33 @@
 #include "variadic_functions.h"
 
 /**
- * print_strings - print number of strings
- * @separator: separate betwen strings
- * @n: number of arguments passed in function
- *
+ * print_strings - prints strings
+ * @separator: separator between strings
+ * @n: number of arguments
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-        unsigned int i;
-        char *str;
-	
-	va_list string;
+	unsigned int i;
+	char *str;
 
-        va_start(string, n);
+	va_list list;
 
-        for(i = 0; i < n; i++)
-        {
-            	str = (va_arg(string, char *));
-                if (!separator)
-		    	printf("%s", str);
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(list, char *);
+		if (!str)
+			str = "(nil)";
+		if (!separator)
+			printf("%s", str);
 		else if (separator && i == 0)
-		    	printf("%s", str);
-		else if (str == NULL)
-			printf("nil");
+			printf("%s", str);
 		else
 			printf("%s%s", separator, str);
-        }
+	}
 
-        va_end (string);
+	printf("\n");
 
-        printf("\n");
+	va_end(list);
 }
