22c66b373d9b60c3c5b9ffc9b7acbace4dd1c2ea
OMAREID0 <omareid221b@gmail.com>
2023-09-14 03:30:02 +0300

    Final

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "calc.h"
+
+//function to add two intger and return result//
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+//function to subtraction two intgers and return result//
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+//function to multiplication two intgers and return result//
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+//function to division two intgers and return result//
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+//function to division two intgers and return reminder result//
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
