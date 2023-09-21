dc744823aba2c2d94157428bf2cb09f05a45d9b1
OMAREID0 <omareid221b@gmail.com>
2023-09-21 22:49:39 +0300

    second

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * list_len - count number of nodes in the list
+ * @h: pointer point to the first node
+ *
+ * Return: return number of the nodes
+ */
+size_t list_len(const list_t *h)
+{
+	int i;
+	size_t count = 0;
+
+	for (i = 0; h != NULL; i++)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
