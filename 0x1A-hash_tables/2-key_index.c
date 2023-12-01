9355744ca76ec25c988194a61edb8905fda9b24f
OMAREID0 <omareid221b@gmail.com>
2023-12-01 20:21:24 +0200

    hash_table

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "header.h"
+
+/**
+ * key_index - returns the index of a key
+ * @key: the key
+ * @size: the size of the array
+ * 
+ * return: the index of the key
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+    unsigned long int index = 0;
+    index = hash_djb2(key) % size;
+    return (index);
+}
---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,23 @@
+#include "header.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @key: the key
+ * @ht: the hash table
+ * 
+ * return: value associated with the element, or NULL if key couldn't be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+    unsigned long int index;
+    
+    index = key_index(key, ht->size);
+    if(index >= ht->size)
+        return (NULL);
+    for (index;ht->array[index] != NULL; index++)
+    {
+        if (strcmp(ht->array[index]->key, key) == 0)
+            return (ht->array[index]->value);
+    }
+    return (NULL);
+}
