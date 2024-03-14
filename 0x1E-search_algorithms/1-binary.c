27ea83e8fe3cc7f06ca8b395832354a5656cc342
OMAREID0 <omareid221b@gmail.com>
2024-03-14 04:46:08 +0200

    betty

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,67 +1,61 @@
 #include "search_algos.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * linear_search - Searches for a value using linear search algorithm
- * @array: Pointer to the first element of the array
- * @size: Number of elements in the array
- * @value: Value to search for
+ * recursive_search - searches for a value in an array of
+ * integers using the Binary search algorithm
  *
- * Return: Index of the value if found, -1 otherwise
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
+int recursive_search(int *array, size_t size, int value)
+{
+	size_t half = size / 2;
+	size_t i;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	printf("Searching in array");
+
+	for (i = 0; i < size; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+
+	printf("\n");
+
+	if (half && size % 2 == 0)
+		half--;
+
+	if (value == array[half])
+		return ((int)half);
+
+	if (value < array[half])
+		return (recursive_search(array, half, value));
+
+	half++;
+
+	return (recursive_search(array + half, size - half, value) + half);
+}
+
+/**
+ * binary_search - calls to binary_search to return
+ * the index of the number
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
  */
 int binary_search(int *array, size_t size, int value)
 {
-if (array == NULL)
-{
-return (-1);
-}
-int left = 0;
-int right = size - 1;
+	int index;
 
-while (left <= right)
-{
-int mid = left + (right - left) / 2;
+	index = recursive_search(array, size, value);
 
-printf("Searching in array: ");
-for (int i = left; i <= right; i++)
-{
-printf("%d", array[i]);
-if (i != right)
-{
-printf(", ");
-}
-}
-printf("\n");
+	if (index >= 0 && array[index] != value)
+		return (-1);
 
-if (array[mid] == value)
-{
-return (mid);
-}
-
-if (array[mid] < value)
-{
-left = mid + 1;
-}
-else
-{
-right = mid - 1;
-}
-}
-
-return (-1);
-}
-
-
-int main(void)
-{
-int array[] = {
-0, 1, 2, 3, 4, 5, 6, 7, 8, 9
-};
-size_t size = sizeof(array) / sizeof(array[0]);
-
-printf("Found %d at index: %d\n\n", 2, binary_search(array, size, 2));
-printf("Found %d at index: %d\n\n", 5, binary_search(array, 5, 5));
-printf("Found %d at index: %d\n", 999, binary_search(array, size, 999));
-return (EXIT_SUCCESS);
+	return (index);
 }
